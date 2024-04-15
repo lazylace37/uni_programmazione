e02298867e6d9585ab5d2a2b59b956aaebd84a32
Gioele Vuaran <vuarangioele@gmail.com>
2024-04-15 09:46:49 +0200

    fix 240409_scacchiera_regine

---
+++ lezioni_teoria/240409_scacchiera_regine/Board.java
@@ -0,0 +1,36 @@
+/**
+ * Board b = new Board();
+ *
+ * <p>b.size() : int
+ *
+ * <p>b.queensOn() : int
+ *
+ * <p>b.underAttach(i, j) : boolean
+ *
+ * <p>b.addQueen(i, j) : Board
+ *
+ * <p>b.arrangement() : String
+ */
+public class Board {
+  public Board(int n) {}
+
+  public int size() {
+    return 0;
+  }
+
+  public int queensOn() {
+    return 0;
+  }
+
+  public boolean underAttach(int i, int j) {
+    return false;
+  }
+
+  public Board addQueen(int i, int j) {
+    return null;
+  }
+
+  public String arrangement() {
+    return "";
+  }
+}
